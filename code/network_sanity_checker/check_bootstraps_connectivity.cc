3321f087cece774c5c14e3b4a28ec25fc24e983e
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2014-12-15 15:25:00 +0000

    Fixed network_sanity_checker to match new NodeId interface.  Added .clang_format and ran against code.  Added missing CMakeLists.txt.

--- code/network_sanity_checker/check_bootstraps_connectivity.cc
+++ code/network_sanity_checker/check_bootstraps_connectivity.cc
@@ -19,37 +19,34 @@ using boost::asio::ip::address;
 namespace {
 
 std::future<int> CanConnect(const udp::endpoint& contact) {
-  return std::async(std::launch::async, [contact]()->int {
-    maidsafe::asymm::Keys keys{ maidsafe::asymm::GenerateKeyPair() };
+  return std::async(std::launch::async, [contact]() -> int {
+    maidsafe::asymm::Keys keys{maidsafe::asymm::GenerateKeyPair()};
     maidsafe::NodeId chosen_bootstrap_peer;
     maidsafe::rudp::NatType nat_type;
     maidsafe::rudp::ManagedConnections managed_connections;
     return managed_connections.Bootstrap(
-        std::vector<udp::endpoint>{ contact },
-        [](const std::string&) {},
+        std::vector<udp::endpoint>{contact}, [](const std::string&) {},
         [](const maidsafe::NodeId&) {},
-        maidsafe::NodeId{ maidsafe::NodeId::IdType::kRandomId },
+        maidsafe::NodeId{maidsafe::RandomString(maidsafe::NodeId::kSize)},
         std::make_shared<maidsafe::asymm::PrivateKey>(keys.private_key),
-        std::make_shared<maidsafe::asymm::PublicKey>(keys.public_key),
-        chosen_bootstrap_peer,
+        std::make_shared<maidsafe::asymm::PublicKey>(keys.public_key), chosen_bootstrap_peer,
         nat_type);
   });
 }
 
-void HandleResult(std::pair<const udp::endpoint, std::future<int>>& result,
-                  bool is_last_result,
+void HandleResult(std::pair<const udp::endpoint, std::future<int>>& result, bool is_last_result,
                   std::ofstream& results_fstream) {
-  std::string closer{ is_last_result ? " }\n" : " },\n" };
+  std::string closer{is_last_result ? " }\n" : " },\n"};
   try {
-    results_fstream << "    { \"contact\": \"" << result.first << "\", \"canConnect\": " << std::boolalpha;
-    int code{ result.second.get() };
+    results_fstream << "    { \"contact\": \"" << result.first
+                    << "\", \"canConnect\": " << std::boolalpha;
+    int code{result.second.get()};
     if (code == maidsafe::rudp::kSuccess) {
       results_fstream << true << closer;
       return;
     }
     std::cout << "Failed to connect to " << result.first << " with return code " << code << '\n';
-  }
-  catch (const std::exception& e) {
+  } catch (const std::exception& e) {
     std::cout << "Exception trying to connect to " << result.first << ": " << e.what() << '\n';
   }
   results_fstream << false << closer;
@@ -59,12 +56,18 @@ void HandleResult(std::pair<const udp::endpoint, std::future<int>>& result,
 
 void CheckBootstrapsConnectivity(std::ofstream& results_fstream) {
   std::map<udp::endpoint, std::future<int>> results;
-  results.emplace(udp::endpoint{ address::from_string("23.239.27.245"),  maidsafe::kLivePort }, std::future<int>{});
-  results.emplace(udp::endpoint{ address::from_string("95.85.32.100"),   maidsafe::kLivePort }, std::future<int>{});
-  results.emplace(udp::endpoint{ address::from_string("104.131.253.66"), maidsafe::kLivePort }, std::future<int>{});
-  results.emplace(udp::endpoint{ address::from_string("106.185.24.221"), maidsafe::kLivePort }, std::future<int>{});
-  results.emplace(udp::endpoint{ address::from_string("128.199.159.50"), maidsafe::kLivePort }, std::future<int>{});
-  results.emplace(udp::endpoint{ address::from_string("178.79.156.73"),  maidsafe::kLivePort }, std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("23.239.27.245"), maidsafe::kLivePort},
+                  std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("95.85.32.100"), maidsafe::kLivePort},
+                  std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("104.131.253.66"), maidsafe::kLivePort},
+                  std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("106.185.24.221"), maidsafe::kLivePort},
+                  std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("128.199.159.50"), maidsafe::kLivePort},
+                  std::future<int>{});
+  results.emplace(udp::endpoint{address::from_string("178.79.156.73"), maidsafe::kLivePort},
+                  std::future<int>{});
 
   for (auto& result : results)
     result.second = CanConnect(result.first);
